96349369646ecacb5779a5691dceffa5ece18297
Olubusayo Amowe <amoweolubusayo@gmail.com>
2022-07-17 17:20:45 +0100

    more words

--- src/dictionary.js
+++ src/dictionary.js
@@ -2,59 +2,247 @@ export const wordList = [
   {
     word: "Aggregators",
     definition:
-      "Aggregators brings together trades across various decentralized finance platforms into one location which in turn saves people time during trading.",
+      "Aggregators brings together trades across various decentralized finance platforms into one location which in turn saves people time during trading",
   },
   {
     word: "Arbitrage",
     definition:
-      "Arbitrage is the practice of profiting from a price gap between various markets by executing a series of identical transactions.",
+      "Arbitrage is the practice of profiting from a price gap between various markets by executing a series of identical transactions",
   },
   {
     word: "Assets",
     definition:
-      "Assets: anything that has the potential to generate positive economic value. Assets are ownership values which can be turned into cash.",
+      "Assets: anything that has the potential to generate positive economic value Assets are ownership values which can be turned into cash",
   },
   {
     word: "Automated Market Maker (AMM)",
-    definition: "They are Liquidity pool providers.",
+    definition: "They are Liquidity pool providers",
   },
   {
     word: "Black Scholes",
     definition:
-      "Black Scholes is a pricing model that uses six variables, including price fluctuations, type of option, underlying price of the stock, time, strike price, and risk-free rate, to ascertain the decent price or predicted value of a call.",
+      "Black Scholes is a pricing model that uses six variables, including price fluctuations, type of option, underlying price of the stock, time, strike price, and risk-free rate, to ascertain the decent price or predicted value of a call",
   },
   {
     word: "Borrowing",
     definition:
-      "Borrowing is taking crypto funds from a DeFi with the intention of repaying it later.",
+      "Borrowing is taking crypto funds from a DeFi with the intention of repaying it later",
   },
   {
     word: "Borrow cap",
     definition:
-      "Borrow cap is the amount of money that can be borrowed at a time which is determined by lenders in the funding pool and collateral factor.",
+      "Borrow cap is the amount of money that can be borrowed at a time which is determined by lenders in the funding pool and collateral factor",
   },
   {
     word: "Bonding Curve",
     definition:
-      "Bonding Curve is a mathematical concept that describes how a price of an asset and supply are associated.",
+      "Bonding Curve is a mathematical concept that describes how a price of an asset and supply are associated",
   },
   {
     word: "CEFI",
-    definition: "CEFI means centralized finance.",
+    definition: "CEFI means centralized finance",
   },
   {
     word: "Cryptocurrency",
     definition:
-      "Cryptocurrency is a digital currency in which transactions are verified and records are kept using cryptography .",
+      "Cryptocurrency is a digital currency in which transactions are verified and records are kept using cryptography ",
   },
   {
     word: "Collateral",
     definition:
-      "Collateral is a borrower's commitment of specific asset in form of crypto to a lender in order to secure repayment of a loan.",
+      "Collateral is a borrower's commitment of specific asset in form of crypto to a lender in order to secure repayment of a loan",
   },
   {
     word: "Collateral factor",
     definition:
-      "Collateral factor is a factor that determines borrow cap of a borrower.",
+      "Collateral factor is a factor that determines borrow cap of a borrower",
+  },
+  {
+    word: "Constant Function Market Makers",
+    definition: "",
+  },
+  {
+    word: "Constant Product Market Makers",
+    definition: "",
+  },
+  {
+    word: "DApp",
+    definition: "",
+  },
+  {
+    word: "Decentralized Exchanges (DEX)",
+    definition: "",
+  },
+  {
+    word: "DEFI",
+    definition: "",
+  },
+  {
+    word: "Derivatives",
+    definition: "",
+  },
+  {
+    word: "ERC-20",
+    definition: "",
+  },
+  {
+    word: "Ethereum",
+    definition: "",
+  },
+  {
+    word: "Fair Value evaluation",
+    definition: "",
+  },
+  {
+    word: "Flashloans",
+    definition: "",
+  },
+  {
+    word: "Fiat",
+    definition: "",
+  },
+  {
+    word: "Front running",
+    definition: "",
+  },
+  {
+    word: "Futures",
+    definition: "",
+  },
+  {
+    word: "Gas",
+    definition: "",
+  },
+  {
+    word: "Hodl",
+    definition: "",
+  },
+  {
+    word: "ICO",
+    definition: "",
+  },
+  {
+    word: "Impermanent Loss",
+    definition: "",
+  },
+  {
+    word: "Lending",
+    definition: "",
+  },
+  {
+    word: "Liquidity",
+    definition: "",
+  },
+  {
+    word: "Liquidity Mining",
+    definition: "",
+  },
+  {
+    word: "Liquidity Pool",
+    definition: "",
+  },
+  {
+    word: "Liquidity Provider",
+    definition: "",
+  },
+  {
+    word: "Liquidity Sensitivity",
+    definition: "",
+  },
+  {
+    word: "Liquidation threshold",
+    definition: "",
+  },
+  {
+    word: "Mempool",
+    definition: "",
+  },
+  {
+    word: "Off-Chain",
+    definition: "",
+  },
+  {
+    word: "Oracle",
+    definition: "",
+  },
+  {
+    word: "Oracle Attacks",
+    definition: "",
+  },
+  {
+    word: "Overcollaterized lending",
+    definition: "",
+  },
+  {
+    word: "Price Slippage",
+    definition: "",
+  },
+  {
+    word: "P2P",
+    definition: "",
+  },
+  {
+    word: "Pump and Dump",
+    definition: "",
+  },
+  {
+    word: "Reserve Factor",
+    definition: "",
+  },
+  {
+    word: "Reward distribution",
+    definition: "",
+  },
+  {
+    word: "Slippage",
+    definition: "",
+  },
+  {
+    word: "Smart Contracts",
+    definition: "",
+  },
+  {
+    word: "Spot",
+    definition: "",
+  },
+  {
+    word: "Stable Coins",
+    definition: "",
+  },
+  {
+    word: "Swapping",
+    definition: "",
+  },
+  {
+    word: "Synthetics",
+    definition: "",
+  },
+  {
+    word: "Synths",
+    definition: "",
+  },
+  {
+    word: "Tokens",
+    definition: "",
+  },
+  {
+    word: "Total Value Locked (TVL)",
+    definition: "",
+  },
+  {
+    word: "Vault",
+    definition: "",
+  },
+  {
+    word: "Yield",
+    definition: "",
+  },
+  {
+    word: "Yield Farming",
+    definition: "",
+  },
+  {
+    word: "Wallet",
+    definition: "",
   },
 ];
